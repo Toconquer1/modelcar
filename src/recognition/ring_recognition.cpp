59514781878357022377483c466abdc93e263307
Zhou hongtao <65892762+Toconquer1@users.noreply.github.com>
2022-08-11 00:08:39 +0800

    Update ring_recognition.cpp
    20220810版本

--- src/recognition/ring_recognition.cpp
+++ src/recognition/ring_recognition.cpp
@@ -1,17 +1,13 @@
 #pragma once
 /**
- * @file cross_recognition.cpp
- * @author Leo (liaotengjun@hotmail.com)
- * @brief 十字道路识别与图像处理
- * @version 0.1
- * @date 2022-03-14
+ * @file ring_recognition.cpp
+ * @author zhht
+ * @brief 环岛
+ * @version 0.0未成功版本
+ * @date 2022-08-10 23：36
  *
  * @copyright Copyright (c) 2022
- *
- * @note 十字道路处理步骤：
- *                      [01] 入十字类型识别：track_recognition.cpp
- *                      [02] 补线起止点搜索
- *                      [03] 边缘重计算
+ * @note  环岛识别，进入补线，出去补线，没有解决出环岛再次识别环岛的问题
  *
  */
 
@@ -52,18 +48,19 @@ public:
         //std判断是左边还是右边
         if(nowStep==RingStep::RingNoneStep)
         {
-            if (track.pointsEdgeLeft.size() < 20 || track.pointsEdgeRight.size() < 20)return false;
+            //if (track.pointsEdgeLeft.size() < 20 || track.pointsEdgeRight.size() < 20)return false;
             
             
-            int i=min(track.pointsEdgeLeft.size()-1,track.pointsEdgeRight.size()-1);
+            
             bool isRing1 = false;
             bool leftloss=false,rightloss=false;
             int cntleft=0,cntright=0;//丢线计数器
-            int edgenum = 5;
+            int edgenum = 10;
+            int i = min(track.pointsEdgeLeft.size() - 1, track.pointsEdgeRight.size() - 1);
             for(;i>=0;--i){
                 if(track.pointsEdgeLeft[i].y<= edgenum)cntleft++;//左边丢线计数器
                 if(track.pointsEdgeRight[i].y>=COLSIMAGE- edgenum)cntright++;//右边丢线计数器
-                if(cntleft>100){
+                if(cntleft>50){
                     if(cntright<10){
                         LorRFlag=RingPosition::Left;
                         isRing1=true;
@@ -71,7 +68,7 @@ public:
                     else isRing1=false;
                     break;
                 }
-                if(cntright>100){
+                if(cntright>50){
                     if(cntleft<10){
                         LorRFlag=RingPosition::Right;
                         isRing1=true;
@@ -80,6 +77,7 @@ public:
                     break;
                 }
             }
+            if(i==0)return false;
             if (isRing1) {
                 nowStep = RingStep::RingEntering;
                 return true;
@@ -90,89 +88,6 @@ public:
 
 
 
-
-
-
-            /*
-            //如果是环岛，低视角
-            //判断逻辑为某一侧先丢线，某一侧后丢线
-            bool isRing1=false,isRing2=false;
-            int i=0;
-            int cntleft=0,cntright=0;
-            int edgenum = 5;
-            for(i=0;i<=track.pointsEdgeLeft.size()-1&&i<=track.pointsEdgeRight.size()-1;++i){
-                if(track.pointsEdgeLeft[i].y<= edgenum &&track.pointsEdgeRight[i].y>=COLSIMAGE- edgenum)continue;//跳过一开始的两侧都丢线
-            }
-            for(;i<=track.pointsEdgeLeft.size()-1&&i<=track.pointsEdgeRight.size()-1;++i){
-                if(track.pointsEdgeLeft[i].y>= edgenum)cntleft++;//左边有线计数器
-                if(track.pointsEdgeRight[i].y<=COLSIMAGE- edgenum)cntright++;
-                i
-            }
-            if(!isRing1)return false;
-
-            cntleft=0,cntright=0;//清空计数器
-            if(LorRFlag==RingPosition::Right){
-                for(;i<=track.pointsEdgeLeft.size()-1&&i<=track.pointsEdgeRight.size()-1;++i){
-                    if(cntleft<10){
-                        if(track.pointsEdgeLeft[i].y>=2)cntleft++;
-                        else cntleft=0;
-                    }
-                    else break;
-                }
-                cntleft=0,cntright=0;
-                for(;i<=track.pointsEdgeLeft.size()-1&&i<=track.pointsEdgeRight.size()-1;++i){
-                    if(track.pointsEdgeLeft[i].y>=2)cntleft++;//左边有线计数器
-                    if(track.pointsEdgeRight[i].y<=COLSIMAGE-2)cntright++;
-                    if(cntleft>40){
-                        if(cntright<15){
-                            isRing2=true;
-                        }
-                        break;
-                    }
-                }
-
-                if(!isRing2){
-                    LorRFlag=RingPosition::RingNonePosition;
-                    return false;
-                }
-
-                //找分叉点
-                //左边的分叉点定为row中间的点,右边的点是最后一次丢线的点
-                int xl=0,yl=0,xr=0,yr=0;
-                int indexl=0,indexr=0;
-                for(i=0;i<=track.pointsEdgeLeft.size()-1&&i<=track.pointsEdgeRight.size()-1;++i){
-                    if(abs(track.pointsEdgeLeft[i].x-ROWSIMAGE/2)<5){
-                        xl=track.pointsEdgeLeft[i].x;
-                        yl=track.pointsEdgeLeft[i].y;
-                        indexl=i;
-                    }
-                    if(track.pointsEdgeRight[i].y>=COLSIMAGE-2){
-                        xr=track.pointsEdgeRight[i].x;
-                        yr=track.pointsEdgeRight[i].y;
-                        indexr=i;
-                    }
-                }
-                int temp=track.pointsEdgeLeft[indexr].y+track.pointsEdgeRight[indexr].y;
-                track.pointsEdgeLeft[indexr].y=temp/2>COLSIMAGE/2?COLSIMAGE/2:temp/2;
-
-                //开始补左线
-                float k = (float)(track.pointsEdgeLeft[indexr].y - track.pointsEdgeLeft[indexl].y) / (float)(track.pointsEdgeLeft[indexr].x - track.pointsEdgeLeft[indexl].x);
-                float b = track.pointsEdgeLeft[indexl].y - k * track.pointsEdgeLeft[indexl].x;
-                for(i=indexl;i<=indexr;++i){
-                    track.pointsEdgeLeft[i].y=(int)(k * track.pointsEdgeLeft[i].x + b);
-                }
-            }
-
-            
-        
-            if(isRing1&&isRing2){
-                nowStep=RingStep::RingEntering;
-                return true;
-            }
-            else{
-                return false;
-            }
-           */
 
         }
         else if(nowStep==RingStep::RingEntering){
@@ -193,7 +108,7 @@ public:
                     }
                 }
                 int temp=track.pointsEdgeLeft[indexr].y+track.pointsEdgeRight[indexr].y;
-                track.pointsEdgeLeft[indexr].y=temp/2>COLSIMAGE/2?COLSIMAGE/2:temp/2;
+                track.pointsEdgeLeft[indexr].y=temp/2>COLSIMAGE/2?COLSIMAGE/2:temp/2;//补线终点
 
                 //开始补左线
                 float k = (float)(track.pointsEdgeLeft[indexr].y - track.pointsEdgeLeft[indexl].y) / (float)(track.pointsEdgeLeft[indexr].x - track.pointsEdgeLeft[indexl].x);
@@ -203,7 +118,7 @@ public:
                 }
             }
             //如果找不到分叉点
-            if(track.spurroad.size()<2){
+            if(track.spurroad.size()<1){
                 nowStep=RingStep::RingEnterFinish;
             }
             return true;
@@ -213,19 +128,41 @@ public:
 
             //如果圆环在右，左测有突变
             //如果圆环在左，右测有突变
-            nowStep=RingStep::RingExiting;
+
+            if(track.stdevLeft>10&&track.stdevRight>10)//脱离弯道
+                nowStep=RingStep::RingExiting;
             return true;
         }
         else if(nowStep=RingStep::RingExiting){
             //应该补线，但是暂时什么都不做
-
-            //如果圆环在右，左测没有突变
-            //如果圆环在左，右测没有突变
-            if(track.stdevLeft<60&&track.stdevRight<60){
-                 nowStep=RingStep::RingNoneStep;
-                LorRFlag=RingPosition::RingNonePosition;
-                return false;
+            //环岛在右，找到左边突变点
+            if(LorRFlag==RingPosition::Right){
+                //找到左边缘的突变点
+                int rowBreakLeftDown=searchBreakLeftDown(track.pointsEdgeLeft);
+                if(rowBreakLeftDown>=track.pointsEdgeLeft.size()-3){
+                    nowStep=RingStep::RingNoneStep;
+                    LorRFlag=RingPosition::RingNonePosition;
+                    return false;
+                }
+                int rowRepair=track.pointsEdgeLeft.size()-1;
+                for(int i=track.pointsEdgeLeft.size()-1;i>=0;--i){
+                    if(track.pointsEdgeLeft[i].y<COLSIMAGE/2){
+                        rowRepair=i;
+                        break;
+                    }
+                }
+                track.pointsEdgeLeft[rowRepair].y=COLSIMAGE/2;
+                //开始补线
+                float k = (float)(track.pointsEdgeLeft[rowRepair].y - track.pointsEdgeLeft[rowBreakLeftDown].y) / (float)(track.pointsEdgeLeft[rowRepair].x - track.pointsEdgeLeft[rowBreakLeftDown].x);
+                float b = track.pointsEdgeLeft[rowBreakLeftDown].y - k * track.pointsEdgeLeft[rowBreakLeftDown].x;
+                for(int i=rowBreakLeftDown;i<=rowRepair;++i){
+                    track.pointsEdgeLeft[i].y=(int)(k * track.pointsEdgeLeft[i].x + b);
+                }
             }
+
+
+
+            
             return true;
            
         }
@@ -268,31 +205,32 @@ public:
     
 private:
    
-    
-    /*
-    bool repairLeftEnable = false;  //左边缘补线使能标志
-    bool repairRightEnable = false; //右边缘补线使能标志
-
-    uint16_t _index = 0;
-    POINT _pointLU;
-    POINT _pointLD;
-    POINT _pointRU;
-    POINT _pointRD;
-    string _text;
-    */
     /**
-     * @brief 十字道路类型
+     * @brief 搜索十字赛道突变行（左下）
      *
+     * @param pointsEdgeLeft
+     * @return uint16_t
      */
-     /*
-    enum CrossroadType
+    uint16_t searchBreakLeftDown(vector<POINT> pointsEdgeLeft)
     {
-        None = 0,
-        CrossroadLeft,     //左斜入十字
-        CrossroadRight,    //右斜入十字
-        CrossroadStraight, //直入十字
-    };
+        uint16_t rowBreakLeftDown = 0;
+        uint16_t counter = 0;
 
-    CrossroadType crossroadType = CrossroadType::None; //十字道路类型
-    */
+        for (int i = 0; i < pointsEdgeLeft.size() - 10; i++) //寻找左边跳变点
+        {
+            if (pointsEdgeLeft[i].y > pointsEdgeLeft[rowBreakLeftDown].y)
+            {
+                rowBreakLeftDown = i;
+                counter = 0;
+            }
+            else if (pointsEdgeLeft[i].y < pointsEdgeLeft[rowBreakLeftDown].y) //突变点计数
+            {
+                counter++;
+                if (counter > 5)
+                    return rowBreakLeftDown;
+            }
+        }
+
+        return rowBreakLeftDown;
+    }
 };
